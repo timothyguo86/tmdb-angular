c83489af5355d6216f176b18e90ae76996cf5816
Timothy Guo <timothyguo86@gmail.com>
2024-02-24 16:28:55 -0500

    [feat] Created Movie and MovieDto types

---
+++ src/app/types/movie.ts
@@ -0,0 +1,23 @@
+export type Movie = {
+  id: number
+  adult: boolean
+  backdrop_path: string
+  genre_ids: number[]
+  original_language: string
+  original_title: string
+  overview: string
+  popularity: number
+  poster_path: string
+  release_date: string
+  title: string
+  video: boolean
+  vote_average: number
+  vote_count: number
+}
+
+export type MoviesDto = {
+  page: number
+  results: Movie[]
+  total_pages: number
+  total_results: number
+}
