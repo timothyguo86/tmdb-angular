8883ad7741c8378ea5b710032fab28160408d2b7
Timothy Guo <timothyguo86@gmail.com>
2024-02-24 12:59:24 -0500

    [feat] Set up router for home component and movie-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,13 @@ import {
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { HeaderComponent } from './shared/header/header.component'
-import { FooterComponent } from './shared/footer/footer.component'
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MovieListComponent } from './pages/movie-list/movie-list.component';
+import { SliderComponent } from './component/slider/slider.component'
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent, MovieListComponent, SliderComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
