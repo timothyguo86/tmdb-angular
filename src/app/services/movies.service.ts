15857957c6b553acfdd31fdab232473a9aa6a80f
Timothy Guo <timothyguo86@gmail.com>
2024-02-25 00:06:40 -0500

    [refactor] moviesService refactor

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -8,21 +8,9 @@ export class MoviesService {
   private apiKey = 'dadb019730c0075868955d1ec94040bb'
   constructor(private http: HttpClient) {}
 
-  getPopularMovies() {
+  getMoviesByType(type: string) {
     return this.http.get<MoviesDto>(
-      `${this.apiUrl}/movie/popular?api_key=${this.apiKey}`
-    )
-  }
-
-  getUpcomingMovies() {
-    return this.http.get<MoviesDto>(
-      `${this.apiUrl}/movie/upcoming?api_key=${this.apiKey}`
-    )
-  }
-
-  getTopRatedMovies() {
-    return this.http.get<MoviesDto>(
-      `${this.apiUrl}/movie/top_rated?api_key=${this.apiKey}`
+      `${this.apiUrl}/movie/${type}?api_key=${this.apiKey}`
     )
   }
 }
