ad6d14b430f3486aa82e89e927a0765df3cb604b
Timothy Guo <timothyguo86@gmail.com>
2024-02-25 11:24:24 -0500

    [feat] Created popularTvshows banner in the home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core'
 import { MoviesService } from '../../services/movies.service'
+import { TvshowsService } from '../../services/tvshows.service'
+import { map } from 'rxjs'
+import { mapToMovies } from '../../types/tvshow'
 
 @Component({
   selector: 'app-home',
@@ -9,5 +12,11 @@ import { MoviesService } from '../../services/movies.service'
 export class HomeComponent {
   upcomingMovies$ = this.moviesService.getMoviesByType('upcoming', 12)
   topRatedMovies$ = this.moviesService.getMoviesByType('top_rated', 12)
-  constructor(private moviesService: MoviesService) {}
+  popularTvshows$ = this.tvshowsService
+    .getTvShowsByType('popular', 12)
+    .pipe(map(mapToMovies))
+  constructor(
+    private moviesService: MoviesService,
+    private tvshowsService: TvshowsService
+  ) {}
 }
