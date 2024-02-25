d28b0c37ca895e650354ea68db0a00ccc629c6c2
Timothy Guo <timothyguo86@gmail.com>
2024-02-25 15:24:11 -0500

    [feat] Create IMAGES_SIZES constant and primary-color global variable

--- src/app/constants/images-sizes.ts
+++ src/app/constants/images-sizes.ts
@@ -1,1 +1,8 @@
 export const imagesBaseUrl = 'https://image.tmdb.org/t/p/'
+
+export const IMAGES_SIZES = {
+  small: `${imagesBaseUrl}/w185`,
+  medium: `${imagesBaseUrl}/w342`,
+  large: `${imagesBaseUrl}/w1280`,
+  original: `${imagesBaseUrl}/original`,
+}
