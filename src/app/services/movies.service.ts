00fc3a9ea1da08928da0852153f63cb5015d86d4
Timothy Guo <timothyguo86@gmail.com>
2024-02-25 13:33:58 -0500

    [feat] Add getMovieById(id to moviesService

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
-import { MoviesDto } from '../types/movie'
+import { Movie, MoviesDto } from '../types/movie'
 import { map } from 'rxjs'
 
 @Injectable()
@@ -13,5 +13,11 @@ export class MoviesService {
     return this.http
       .get<MoviesDto>(`${this.apiUrl}/movie/${type}?api_key=${this.apiKey}`)
       .pipe(map((data) => data.results.slice(0, count)))
+  }
+
+  getMovieById(id: string) {
+    return this.http.get<Movie>(
+      `${this.apiUrl}/movie/${id}?api_key=${this.apiKey}`
+    )
   }
 }
