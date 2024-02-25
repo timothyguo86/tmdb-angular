75152f650cf47bab80d751f9b540c144deddc65f
Timothy Guo <timothyguo86@gmail.com>
2024-02-25 11:22:10 -0500

    [feat] Created Tvshow and TvshowsDto types

---
+++ src/app/types/tvshow.ts
@@ -0,0 +1,37 @@
+import { Movie } from './movie'
+
+export type Tvshow = {
+  id: number
+  adult: boolean
+  backdrop_path: string
+  first_air_date: string
+  genre_ids: number[]
+  name: string
+  original_country: string[]
+  original_language: string
+  original_name: string
+  overview: string
+  popularity: number
+  poster_path: string
+  release_date: string
+  video: boolean
+  vote_average: number
+  vote_count: number
+}
+
+export type TvshowsDto = {
+  page: number
+  results: Tvshow[]
+  total_pages: number
+  total_results: number
+}
+
+export function mapToMovies(tvshows: Tvshow[]): Movie[] {
+  return tvshows.map((tvshow: Tvshow) => {
+    return {
+      ...tvshow,
+      title: tvshow.name,
+      original_title: tvshow.original_name,
+    }
+  })
+}
