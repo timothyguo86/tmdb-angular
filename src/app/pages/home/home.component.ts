85528480bb32c6e4acd196d852f5763426cd2cf0
Timothy Guo <timothyguo86@gmail.com>
2024-02-25 00:01:22 -0500

    [feat] Added upcoming movies banner and top rated movie banners to the home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { MoviesService } from '../../services/movies.service'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
+  upcomingMovies$ = this.moviesService.getUpcomingMovies()
+  topRatedMovies$ = this.moviesService.getTopRatedMovies()
+  constructor(private moviesService: MoviesService) {}
 }
