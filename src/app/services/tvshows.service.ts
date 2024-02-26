6b6492fb7197e847e503d389c1464cf9a82149bf
Timothy Guo <timothyguo86@gmail.com>
2024-02-25 21:28:26 -0500

    [feat] Updated tvshos service to fetch tv show data

--- src/app/services/tvshows.service.ts
+++ src/app/services/tvshows.service.ts
@@ -1,7 +1,10 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { map } from 'rxjs'
-import { TvshowsDto } from '../types/tvshow'
+import { Tvshow, TvshowsDto } from '../types/tvshow'
+import { CreditsDto } from '../types/creedits'
+import { ImagesDto } from '../types/image'
+import { VideosDto } from '../types/video'
 
 @Injectable()
 export class TvshowsService {
@@ -13,5 +16,35 @@ export class TvshowsService {
     return this.http
       .get<TvshowsDto>(`${this.apiUrl}/tv/${type}?api_key=${this.apiKey}`)
       .pipe(map((data) => data.results.slice(0, count)))
+  }
+
+  getSimilarTvshows(id: string, count = 20) {
+    return this.http
+      .get<TvshowsDto>(`${this.apiUrl}/tv/${id}/similar?api_key=${this.apiKey}`)
+      .pipe(map((data) => data.results.slice(0, count)))
+  }
+
+  getTvshowById(id: string) {
+    return this.http.get<Tvshow>(
+      `${this.apiUrl}/tv/${id}?api_key=${this.apiKey}`
+    )
+  }
+
+  getTvshowVideos(id: string) {
+    return this.http
+      .get<VideosDto>(`${this.apiUrl}/tv/${id}/videos?api_key=${this.apiKey}`)
+      .pipe(map((data) => data.results))
+  }
+
+  getTvshowImages(id: string) {
+    return this.http
+      .get<ImagesDto>(`${this.apiUrl}/tv/${id}/images?api_key=${this.apiKey}`)
+      .pipe(map((data) => data.backdrops))
+  }
+
+  getTvshowCast(id: string) {
+    return this.http
+      .get<CreditsDto>(`${this.apiUrl}/tv/${id}/credits?api_key=${this.apiKey}`)
+      .pipe(map((data) => data.cast))
   }
 }
